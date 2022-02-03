7a45a7405d565e3843aa5c024ed4ad26e9a3e6ba
gabicantarini <gabilana64@hotmail.com>
2022-02-03 20:44:43 +0000

    feat: adiciona sistemas

--- CantaBank/Funcionarios/Desenvolvedor.cs
+++ CantaBank/Funcionarios/Desenvolvedor.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace CantaBank.Funcionarios
 {
-    internal class Desenvolvedor
+    public class Desenvolvedor : Funcionario
     {
+
+        public Desenvolvedor(string cpf) : base(3000, cpf)
+        {
+
+        }
+
+        public override void AumentarSalario()
+        {
+
+            Salario *= 1.15;
+        }
+
+        public override double GetBonificacao()
+        {
+            return Salario * 0.01;
+        }
     }
 }
