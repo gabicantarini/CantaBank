ab408457649d1ee68053ddcf0c1834d0b6be8300
gabicantarini <gabilana64@hotmail.com>
2022-02-23 06:14:01 +0000

    feat: bug solution

--- CantaBank/ParceiroComercial.cs
+++ CantaBank/ParceiroComercial.cs
@@ -10,7 +10,7 @@ namespace CantaBank
     public class ParceiroComercial : IAutenticavel
     {
         public string Senha { get; set; }
-        public bool Autenticar (string senha)
+        public bool Autenticar(string senha)
         {
             return Senha == senha;
         }
