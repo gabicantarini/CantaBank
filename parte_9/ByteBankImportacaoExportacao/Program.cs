0e46de0756b6527556caf4df710942876f00b45b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2022-06-23 12:03:55 +0100

    c# concepts in a bank project

---
+++ parte_9/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,26 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // IO = Input e Output
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program  //partial q dizer que temos uma classe com métodos separados em vários arquivos
+    {
+        static void Main(string[] args)
+        {
+            UsarStreamDeEntrada();
+            Console.WriteLine("Aplicação finalizada...");
+            
+
+            Console.ReadLine();
+        }
+               
+
+
+    }
+}
